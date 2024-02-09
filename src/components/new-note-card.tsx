656b2a3c6d559187048a04ebf6985ed6312ede54
isaias <isaiashsobral@gmail.com>
2024-02-09 12:15:58 -0300

    class 1 completed, added more style to notes cards and animation

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,11 +1,11 @@
 export function NewNoteCard() {
   return (
-    <div className="bg-slate-700 rounded-md p-5 text-sm space-y-3">
+    <button className="bg-slate-700 flex flex-col text-start rounded-md p-5 text-sm space-y-3 outline-none hover:ring-1 hover:ring-slate-600 focus-visible:ring-1 focus-visible:ring-lime-400 active:brightness-75 active:translate-y-0.5 transition-all">
       <span className="font-medium text-slate-200">Adicionar Nota</span>
 
       <p className="text-slate-400 leading-6">
         Grave uma nota em áudio que será convertida para texto automaticamente.
       </p>
-    </div>
+    </button>
   );
 }
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="bg-slate-800 rounded-md p-5 text-sm space-y-3 overflow-hidden relative pointer-events-none">
+    <button className="bg-slate-800 flex flex-col text-left rounded-md p-5 text-sm space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-1 focus-visible:ring-lime-400 active:scale-[.98] active:brightness-125 transition-all">
       <span className="font-medium text-slate-300">há 2 dias</span>
 
       <p className="text-slate-400 leading-6">
@@ -10,7 +10,7 @@ export function NoteCard() {
         nesciunt!
       </p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-    </div>
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none " />
+    </button>
   );
 }
